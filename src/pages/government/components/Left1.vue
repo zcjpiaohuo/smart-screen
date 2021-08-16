577aac507124e0862b62c2b6d44d0a0dadc4b625
happy15751 <1575162658@qq.com>
2021-08-16 18:43:40 +0800

    更新

--- src/pages/government/components/Left1.vue
+++ src/pages/government/components/Left1.vue
@@ -2,11 +2,9 @@
  * @Description: 
  * @Author: charles
  * @Date: 2021-05-05 22:02:56
- * @LastEditors: charles
- * @LastEditTime: 2021-07-14 15:18:58
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-16 18:41:29
 -->
 <template>
-  <div>
-    Left1
-  </div>
+  lift1
 </template>
